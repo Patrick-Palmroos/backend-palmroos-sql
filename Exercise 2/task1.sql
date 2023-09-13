6a81bbe5c7c4d59a85054d685ed09886b5e0dd97
Patrick-Palmroos <palmroos.patrick@gmail.com>
2023-09-13 11:37:41 +0300

    e2 t1-t2

--- Exercise 2/task1.sql
+++ Exercise 2/task1.sql
@@ -3,3 +3,8 @@ Listaa ruokalajin "Lihapiirakka" kaikki ainesosat.
 
 List all the ingredients which are used in the dish "Lihapiirakka".
 */
+
+SELECT i.name
+FROM ingredients AS i INNER JOIN contains AS c
+ON c.ingredient_id = i.id INNER JOIN dishes AS d
+ON d.id = c.dish_id WHERE d.name = "Lihapiirakka";
--- Exercise 2/task2.sql
+++ Exercise 2/task2.sql
@@ -3,3 +3,8 @@ Listaa kaikki ruokalajit, joissa on käytetty ainesosaa "Peruna".
 
 List all the dishes which contains ingredient "Peruna".
 */
+
+SELECT d.name
+FROM ingredients AS i INNER JOIN contains AS c
+ON c.ingredient_id = i.id INNER JOIN dishes AS d
+ON d.id = c.dish_id WHERE i.name = "Peruna";
